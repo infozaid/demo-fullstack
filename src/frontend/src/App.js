dd0c0a727239693725c76f6d95ebd264eb3b000b
infozaid <infozaidamin@gmail.com>
2023-09-22 17:52:02 +0500

    apply maven frontend deployment in pom.xml and apply spin on before loading data

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,16 +1,17 @@
 //import logo from './logo.svg';
-import {useState,useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import './App.css';
 import { getAllsStudents } from './client';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
-import { Table } from 'antd';
+import { Breadcrumb, Layout, Menu, theme ,Table,Spin,Empty } from 'antd';
+
 
 import {
-  DesktopOutlined,
-  FileOutlined,
-  PieChartOutlined,
-  TeamOutlined,
-  UserOutlined,
+    DesktopOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    UserOutlined,
+    LoadingOutlined
 } from '@ant-design/icons';
 
 
@@ -18,150 +19,156 @@ import {
 const { Header, Content, Footer, Sider } = Layout;
 
 function getItem(label, key, icon, children) {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  };
+    return {
+        key,
+        icon,
+        children,
+        label,
+    };
 }
 const items = [
-  getItem('Option 1', '1', <PieChartOutlined />),
-  getItem('Option 2', '2', <DesktopOutlined />),
-  getItem('User', 'sub1', <UserOutlined />, [
-    getItem('Tom', '3'),
-    getItem('Bill', '4'),
-    getItem('Alex', '5'),
-  ]),
-  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-  getItem('Files', '9', <FileOutlined />),
+    getItem('Option 1', '1', <PieChartOutlined />),
+    getItem('Option 2', '2', <DesktopOutlined />),
+    getItem('User', 'sub1', <UserOutlined />, [
+        getItem('Tom', '3'),
+        getItem('Bill', '4'),
+        getItem('Alex', '5'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Files', '9', <FileOutlined />),
 ];
 
 
 
 
-const columns =  [
-  
-  {
-      title: 'Id',
-      dataIndex: 'id',
-      key: 'id',
-  },
-  {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-  },
-  {
-      title: 'Email',
-      dataIndex: 'email',
-      key: 'email',
-  },
-  {
-      title: 'Gender',
-      dataIndex: 'gender',
-      key: 'gender',
-  }
+const columns = [
+
+    {
+        title: 'Id',
+        dataIndex: 'id',
+        key: 'id',
+    },
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+    },
+    {
+        title: 'Gender',
+        dataIndex: 'gender',
+        key: 'gender',
+    }
 ];
 
-
-
+const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 
 
 
 function App() {
 
-  
-  const [students,setStudents] = useState([]);
-  const[collapsed,setCollapsed] = useState(false);
 
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
-  
-  const fetchStudents = () =>
-  getAllsStudents().then(res=>res.json())
-  .then(data => {
-       console.log(data);
-       setStudents(data);
-  })
+    const [students, setStudents] = useState([]);
+    const [collapsed, setCollapsed] = useState(false);
+    const [fetching,setFetching] = useState(true);
 
-  useEffect(()=>{
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
+
+    const fetchStudents = () =>
+        getAllsStudents().then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setStudents(data);
+                setFetching(false);
+            })
+
+    useEffect(() => {
         console.log("Component is mounted");
         fetchStudents();
-  },[]);
+    }, []);
 
 
-  const renderStudents = () => {
+    const renderStudents = () => {
 
-    if(students.length<=0){
-      return <p>No Data found</p>
+        if(fetching){
+            return <Spin indicator={antIcon} />;
+        }
+
+        if (students.length <= 0) {
+            return <Empty />;
+        }
+
+        return <Table
+            columns={columns}
+            dataSource={students}
+            bordered
+            title={() => 'Student'}
+            pagination={{ pageSize: 50, }}
+            scroll={{ y: 240, }}
+        />
     }
 
-    return <Table
-    columns={columns}
-    dataSource={students}
-    bordered
-    title={() => 'Header'}
-    footer={() => 'Footer'}
-  />
-  }
 
-  
 
-/*return students.map((student,index)=>{
+    /*return students.map((student,index)=>{
 
-    return <h1 key={index}>{student.id} {student.name} {student.gender}</h1>
+        return <h1 key={index}>{student.id} {student.name} {student.gender}</h1>
 
-  });*/
-  
+      });*/
 
-  return (
-    <Layout
-      style={{
-        minHeight: '100vh',
-      }}
-    >
-      <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
-        <div className="demo-logo-vertical" />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
-      </Sider>
-      <Layout>
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-          }}
-        />
-        <Content
-          style={{
-            margin: '0 16px',
-          }}
-        >
-          <Breadcrumb
+
+    return (
+        <Layout
             style={{
-              margin: '16px 0',
+                minHeight: '100vh',
             }}
-          >
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-          </Breadcrumb>
-
-          <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
-                 {renderStudents()}
-          </div>
-        </Content>
-        <Footer
-          style={{
-            textAlign: 'center',
-          }}
         >
-          Created By ZAID 2023.
-        </Footer>
-      </Layout>
-    </Layout>
-  );
-  
+            <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
+                <div className="demo-logo-vertical" />
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+            </Sider>
+            <Layout>
+                <Header
+                    style={{
+                        padding: 0,
+                        background: colorBgContainer,
+                    }}
+                />
+                <Content
+                    style={{
+                        margin: '0 16px',
+                    }}
+                >
+                    <Breadcrumb
+                        style={{
+                            margin: '16px 0',
+                        }}
+                    >
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+
+                    <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                        {renderStudents()}
+                    </div>
+                </Content>
+                <Footer
+                    style={{
+                        textAlign: 'center',
+                    }}
+                >
+                    Created By ZAID 2023.
+                </Footer>
+            </Layout>
+        </Layout>
+    );
+
 }
 
 export default App;
